89ec0b3cf854ed8b2eb35e77dfd20d54c50122de
Quinten Clause <quintenclause@hotmail.com>
2017-03-30 14:17:50 +0200

    Delete heartbeatmanifest.php

--- heartbeatmanifest.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: text/cache-manifest;");
-echo "CACHE MANIFEST\r\n";
-echo "# v7\r\n";
-echo "CACHE:\r\n";
-echo "index.html\r\n";
-echo "heartbeat_icon.png\r\n";
-echo "heartbeat_favicon.png\r\n";
-?>
